850154338822a8630d17444fe31f5f7313cb79e9
yannick123456780 <yannnick.mpega@gmail.com>
2026-08-07 15:32:05 +0200

    Mise à jour

--- src/Components/SecondaryMenu.tsx
+++ src/Components/SecondaryMenu.tsx
@@ -6,27 +6,20 @@ export default function SecondaryMenu() {
     <div className="flex gap-6">
       {Object.entries(categoriesConfig).map(([slug, cat]) => (
         <div key={slug} className="group relative">
-          <Link to={`/${slug}`}>{cat.label}</Link>
+          <Link to={`/${slug}`}>
+            {cat.label}
+          </Link>
 
           <div className="absolute hidden group-hover:block bg-white shadow p-4">
             {Object.entries(cat.subcategories).map(([subSlug, sub]) => (
               <div key={subSlug}>
-                <Link to={`/${slug}/${subSlug}`}>{sub.label}</Link>
-
-                <div>
-                  {sub.types?.length > 0 && (
-                    <div>
-                      {sub.types.map((type) => (
-                        <Link key={type} to={`/${slug}/${subSlug}/${type}`}>
-                          {type}
-                        </Link>
-                      ))}
-                    </div>
-                  )}
-                </div>
+                <Link to={`/${slug}/${subSlug}`}>
+                  {sub.label}
+                </Link>
               </div>
             ))}
           </div>
+
         </div>
       ))}
     </div>
